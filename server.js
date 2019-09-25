5b310106a4d9c91814835df2cf083f80af78afbc
Tanner Galloway <tannergalloway15@gmail.com>
2019-09-24 18:40:58 -0700

    added procfile

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 // create port
-const PORT = process.env.port || 8080;
+const PORT = process.env.port || 3000;
 
 // express middleware 
 app.use(express.static(path.join(__dirname, '/public')));

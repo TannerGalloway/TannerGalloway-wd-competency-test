189485f5b53ac8b7041a83c3c3e7be683a6727b1
Tanner Galloway <tannergalloway15@gmail.com>
2019-09-29 21:04:39 -0700

    fixed create article error

--- routes/routes.js
+++ routes/routes.js
@@ -277,12 +277,12 @@ router.post("/create", (req, res) => {
     }else{
       // check if article has been created already.
       var titleExist = userArticles.some((article) => {
-        return res.send(article.dataValues.title === title);
+        return article.dataValues.title === title;
       });
       var contentExist = userArticles.some((article) => {
-        return res.send(article.dataValues.content === content);
+        return article.dataValues.content === content;
       });
-      // if article has not been created create article.
+       // if article has not been created create article.
       if(titleExist || contentExist){
         return res.send(false);
       }else{
